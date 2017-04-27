9c22e3be3bab84743327bfa70c8b728eb2b4a9b4
Alex Matos da Silva <alex.matos@ufms.br>
2017-04-27 13:39:51 -0400

    datatable com muitas propriedades

--- Ordenar.js
+++ Ordenar.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
     var table = $('#cronogramaTable').DataTable( {
         rowReorder: true,
-            } );
+        "paging": false,
+        "ordering": false,
+        "info": false,
+        "bFilter": false,
+        "scrollXY": false,
+        "scrollCollapse": true,
+        "processing": true,
+        "responsive": true,
+        "language": {
+            "loadingRecords": "Carregando...",
+            "processing": '<div class = "alert alert-warning"><i class="fa fa-cog fa-spin fa-2x"> </i> Carregando as Pessoas/Instituições Relacionadas ao documento</div>',
+            "sEmptyTable": '<h3>Nenhum registro encontrado</h3>'
+        },
+        "bAutoWidth": false,
+        "aoColumnDefs": [     
+            {"mData": "id", "visible": false}, 
+            
+            {"mData": "etapa",  
+                "mRender": function (data, type, full) {               
+                    return  (data < 10) ? '00' + data :  (data < 100 ? '0' + data : data);
+                }
+            }, 
+            {"mData": "especificacao"}, 
+            {"mData": "quantidade"}, 
+            {"mData": "unidade"}, 
+            {"mData": "data_inicio"}, 
+            {"mData": "data_termino"}, 
+            {"mData": "actions"}
+        ]  
+    } );
 } );
 
 
